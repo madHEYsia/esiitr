52a4f65b66ab6973af9b6fc532718b613d8dd555
madHEYsia <submiitr07@gmail.com>
2018-03-11 04:38:09 +0530

    feature - demo sample working

--- server.js
+++ server.js
@@ -29,16 +29,6 @@ router.use(function(req, res, next) {
     next();
 });
 
-// var options = {
-// 	  args: ['salt']
-// };
-
-// PythonShell.run('C:/Users/submi/Desktop/wikipedia-question-generator/wikitrivia/scripts/wikitrivia.py', options, function (err, results) {
-//   if (err) throw err;
-//   // results is an array consisting of messages collected during execution
-//   console.log(results);
-// });
-
 // -----------------------------------------------------------------------------
 
 var home = router.route('/');
@@ -52,85 +42,25 @@ a validation everytime route /api/user/:user_id it hit.
 
 var home = router.route('/');
 home.get(function(req,res,next){
-  res.render('index');
+  res.render('index', {data: ""});
 });
 
 var pythonApi = router.route('/wiki');
 pythonApi.get(function(req,res,next){
-	console.log(req.params);
-	console.log(req.query);
-	res.send("Hi it's working");
+	var options = {
+		args: req.query.s.split(" ")		
+	};
+
+	PythonShell.run('C:/Users/submi/Desktop/wikipedia-question-generator/wikitrivia/scripts/wikitrivia.py', options, function (err, results) {
+	console.log(results);
+
+
+	if (err) 
+		res.render('index', {data: ""});
+	else
+		res.render('index', {data: results});
+	});
 });
-
-
-// home.get(function(req,res,next){
-
-//     var user_id = req.params.user_id;
-//     res.render('index');
-
-//     req.getConnection(function(err,conn){
-
-//         if (err) return next("Cannot Connect");
-
-//         var query = conn.query("SELECT * FROM t_user WHERE user_id = ? ",[user_id],function(err,rows){
-
-//             if(err){
-//                 console.log(err);
-//                 return next("Mysql error, check your query");
-//             }
-
-//             //if user not found
-//             if(rows.length < 1)
-//                 return res.send("User Not found");
-
-//             res.render('edit',{title:"Edit user",data:rows});
-//         });
-//     });
-
-// });
-
-// var nearby = router.route('/nearby');
-
-// nearby.get(function(req,res,next){
-
-//     var x = req.query.latitude;
-//     var y = req.query.longitude;
-//     var z = req.query.filter;
-//     var isMale = req.query.isMale;
-//     var isFemale = req.query.isFemale;
-//     var isUnisex = req.query.isUnisex;
-//     var isAccessibilityFeature = req.query.isAccessibilityFeature;
-//     var isParkingAvailable = req.query.isParkingAvailable;
-//     var isAdultChangeFacilityAvailable = req.query.isAdultChangeFacilityAvailable;
-//     var isWaterSuitableForDrinking = req.query.isWaterSuitableForDrinking;
-
-//     //inserting into mysql
-//     req.getConnection(function (err, conn){
-
-//         if (err) return next("Cannot Connect");
-
-//         if(z=='nearest'){
-//           var query = conn.query("Select *, (( "+x+" -latitude)*( "+x+" -latitude) + ( "+y+" -longitude)*( "+y+" -longitude)) as distance from janshauch.toilet Where (( "+x+" -latitude)*( "+x+" -latitude) + ( "+y+" -longitude)*( "+y+" -longitude)) < 0.014*0.014  AND isMale>="+isMale+" AND isFemale>="+isFemale+" AND isUnisex>="+isUnisex+" AND isAccessibilityFeature>="+isAccessibilityFeature+" AND isParkingAvailable>="+isParkingAvailable+" AND isAdultChangeFacilityAvailable>="+isAdultChangeFacilityAvailable+" AND isWaterSuitableForDrinking>="+isWaterSuitableForDrinking+" ORDER BY distance desc", function(err, rows){
-//            if(err){
-//                 console.log(err);
-//                 return next("Mysql error, check your query");
-//            }
-//           res.send(rows);
-//         });
-//         }
-//         else{
-//           var query = conn.query("Select *, (( "+x+" -T.latitude)*( "+x+" -T.latitude) + ( "+y+" -T.longitude)*( "+y+" -T.longitude)) as distance from janshauch.toilet as T, janshauch.feedback as R Where (( "+x+" -T.latitude)*( "+x+" -T.latitude) + ( "+y+" -T.longitude)*( "+y+" -T.longitude)) < 0.014*0.014  AND T.latitude=R.latitude  AND T.longitude=R.longitude  AND isMale>="+isMale+" AND isFemale>="+isFemale+" AND isUnisex>="+isUnisex+" AND isAccessibilityFeature>="+isAccessibilityFeature+" AND isParkingAvailable>="+isParkingAvailable+" AND isAdultChangeFacilityAvailable>="+isAdultChangeFacilityAvailable+" AND isWaterSuitableForDrinking>="+isWaterSuitableForDrinking+" ORDER BY distance desc", function(err, rows){
-//             // Select *, (21.25-T.latitude)*(21.25-T.latitude) + (81.62-T.longitude)*(81.62-T.longitude) as distance from janshauch.toilet as T, janshauch.feedback as R Where (21.25-T.latitude)*             (21.25-T.latitude) + (81.62-T.longitude)*(81.62-T.longitude) <  0.014*0.014  AND T.latitude=R.latitude  AND T.longitude=R.longitude ORDER BY distance desc;          
-//            if(err){
-//                 console.log(err);
-//                 return next("Mysql error, check your query");
-//            }
-//           res.send(rows);
-//         });
-//         }
-//      });
-
-// });
 
 //now we need to apply our router here
 app.use(router);

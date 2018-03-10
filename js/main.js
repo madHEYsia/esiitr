9c4884d8e89ec7c9bab5eb77d73a8e84845ffaf5
madHEYsia <submiitr07@gmail.com>
2018-03-10 16:05:38 +0530

    junk - logo and file updates

--- js/main.js
+++ js/main.js
@@ -130,7 +130,7 @@
             e.stopPropagation();
         });
             
-        searchField.attr({placeholder: 'Type Keywords', autocomplete: 'off'});
+        searchField.attr({placeholder: 'Type topics', autocomplete: 'off'});
     
     };
 
